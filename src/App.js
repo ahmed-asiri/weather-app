f1225741d5874297ae6b96bfef63aecd9cb5d6a2
ahmed-asiri <ahmed.su.asiri@gmail.com>
2022-07-12 19:00:34 +0300

    adding typescript to react

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="bg-red-800 text-white">Hello world</div>;
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App(props) {
+  return (
+    <div className="container bg-slate-400 min-h-screen m-auto p-4">
+      <div className="flex">
+        <div className="card"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

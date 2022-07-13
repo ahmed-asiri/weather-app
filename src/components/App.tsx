cda6678ad737e438865e89cc2ed0c11e0716d263
ahmed-asiri <ahmed.su.asiri@gmail.com>
2022-07-13 03:05:41 +0300

    adding background images

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 function App(props: any) {
-  return (
-    <div className="container bg-slate-400 min-h-screen m-auto p-4">
-      <div className="flex">
-        <div className="card"></div>
-      </div>
-    </div>
-  );
+  return <div className="container  min-h-screen m-auto p-8">as</div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "card-color": "#1794F4",
+      },
+    },
   },
   plugins: [],
 };

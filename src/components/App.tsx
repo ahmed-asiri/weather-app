79a4b2cb5d6f61897331a1b7f6b03ee8ea5385a6
ahmed-asiri <ahmed.su.asiri@gmail.com>
2022-07-13 03:09:56 +0300

    create weather detailed card component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import DetailedWeatherCard from "./DetailedWeatherCard";
 
 function App(props: any) {
-  return <div className="container  min-h-screen m-auto p-8">as</div>;
+  return (
+    <div className="container  min-h-screen m-auto py-8">
+      <DetailedWeatherCard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/DetailedWeatherCard.tsx
@@ -0,0 +1,5 @@
+function DetailedWeatherCard(props: any) {
+  return <div>Detailed Component</div>;
+}
+
+export default DetailedWeatherCard;
